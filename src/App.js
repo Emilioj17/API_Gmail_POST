72269042a96f656925821d5e1c78f5a5452fb902
Emilioj17 <ejjelves@gmail.com>
2021-07-12 09:28:56 -0400

    Version integra front y backend

--- src/App.js
+++ src/App.js
@@ -4,25 +4,45 @@ import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
   const [email, setEmail] = useState({
-    "Para": "",
-    "Asunto": "",
-    "Contenido":""
+    "to": "",
+    "subject": "",
+    "body":""
   });
 
-  async function sendEmail(e, url = "", data = {}) {
+  function sendEmail(e) {
     e.preventDefault();
-    // console.log(email);
-
-    const response = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(data)
-    });
-    console.log("Hola desde createData");
-    return response.json();
+    console.log("Hola desde sendEmail");
+    console.log(email);
+    fetch("http://127.0.0.1:5000/enviarCorreo", {
+					method: "POST",
+					body: JSON.stringify(email)
+				})
+					// .then(res => res.json())
+					// .then(data => {
+					// 	setStore({ usuarioActual: [data.nombre, data.correo] });
+					// 	setStore({ ingreso: ["uno"] });
+					// }
+					// ).catch(error => {
+					// 	console.error("Hay un problemilla", error);
+					// }
+					// )
   }
+  
+
+  // async function sendEmail(e, url = "127.0.0.1:5000/enviarCorreo", data = email) {
+  //   e.preventDefault();
+  //   console.log(email);
+
+  //   const response = await fetch(url, {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json"
+  //     },
+  //     body: JSON.stringify(data)
+  //   });
+  //   console.log("Hola desde createData");
+  //   return response.json();
+  // }
 
   return (
     <div className="App">
@@ -30,15 +50,15 @@ function App() {
         <form>
           <div className="mb-3">
             <label for="exampleInputEmail1" className="form-label">Para:</label>
-            <input type="email" onChange={(e) => setEmail({ ...email, Para: e.target.value })} className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+            <input type="email" onChange={(e) => setEmail({ ...email, to: e.target.value })} className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
           </div>
           <div className="mb-3">
             <label for="exampleInputPassword1" className="form-label">Asunto</label>
-            <input type="text" onChange={(e) => setEmail({ ...email, Asunto: e.target.value })} className="form-control" id="exampleInputPassword1"/>
+            <input type="text" onChange={(e) => setEmail({ ...email, subject: e.target.value })} className="form-control" id="exampleInputPassword1"/>
           </div>
           <div className="mb-3">
             <label for="exampleInputPassword1" className="form-label">Contenido</label>
-            <input type="text" onChange={(e) => setEmail({ ...email, Contenido: e.target.value })} className="form-control" id="exampleInputPassword1"/>
+            <input type="text" onChange={(e) => setEmail({ ...email, body: e.target.value })} className="form-control" id="exampleInputPassword1"/>
           </div>
           <button type="buton" className="btn btn-primary" onClick={sendEmail}>Enviar</button>
         </form>
--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
 import json
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 
 from Google import Create_Service
 import base64
@@ -7,9 +8,10 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
 app = Flask(__name__)
+CORS(app)
 
 
-def sendEmail():
+def sendEmail(to, subject, body):
     CLIENT_SECRET_FILE = 'client_secret.json'
     API_NAME = 'gmail'
     API_VERSION = 'v1'
@@ -17,11 +19,11 @@ def sendEmail():
 
     service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
 
-    emailMsg = 'Hey Esto es una Prueba'
+    emailMsg = body
     mimeMessage = MIMEMultipart()
-    mimeMessage['to'] = 'ejjelves@gmail.com; ventaschile314@gmail.com'
-    mimeMessage['Cc'] = 'contacto@denegocios.cl'
-    mimeMessage['subject'] = 'Test'
+    mimeMessage['to'] = to
+    # mimeMessage['Cc'] = 'contacto@denegocios.cl'
+    mimeMessage['subject'] = subject
     mimeMessage.attach(MIMEText(emailMsg, 'plain'))
     raw_string = base64.urlsafe_b64encode(mimeMessage.as_bytes()).decode()
 
@@ -37,16 +39,8 @@ def enviar():
     to = decoded_object["to"]
     subject = decoded_object["subject"]
     body = decoded_object["body"]
-    # persona = Persona()
-    # persona.nombre = nombre
-    # persona.altura = altura
-    # persona.masa = masa
-    # persona.descripcion = descripcion
 
-    # db.session.add(persona)
-    # db.session.commit()
-
-    sendEmail()
+    sendEmail(to, subject, body)
 
     return jsonify("Prueba Correcta"), 201
 
